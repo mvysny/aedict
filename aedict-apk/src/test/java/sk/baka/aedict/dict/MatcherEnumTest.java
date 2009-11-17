6a194bc63a6a96e19201c079cf005c3a1bfd844c
Martin Vysny <vysny@baka.sk>
2009-11-17 17:28:26 +0100

    Fixed test class placement

--- aedict-apk/src/test/java/sk/baka/aedict/dict/MatcherEnumTest.java
+++ aedict-apk/src/test/java/sk/baka/aedict/dict/MatcherEnumTest.java
@@ -16,7 +16,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package sk.baka.aedict;
+package sk.baka.aedict.dict;
 
 import org.junit.Test;
 
--- aedict-apk/src/test/java/sk/baka/aedict/kanji/RomanizationEnumTest.java
+++ aedict-apk/src/test/java/sk/baka/aedict/kanji/RomanizationEnumTest.java
@@ -16,7 +16,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package sk.baka.aedict;
+package sk.baka.aedict.kanji;
 
 import static org.junit.Assert.assertEquals;
 
