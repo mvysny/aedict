15ff8d35454414b64da94c52a7021db4f69985f9
Martin Vysny <vysny@baka.sk>
2010-05-23 18:12:36 +0200

    Added utility methods for checks

---
+++ aedict-apk/src/main/java/sk/baka/aedict/util/Check.java
@@ -0,0 +1,35 @@
+/**
+ *     Aedict - an EDICT browser for Android
+ Copyright (C) 2009 Martin Vysny
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sk.baka.aedict.util;
+
+/**
+ * Provides check methods
+ * 
+ * @author Martin Vysny
+ */
+public class Check {
+	private Check() {
+		throw new AssertionError();
+	}
+
+	public static void checkNotNull(final String name, final Object value) {
+		if (value == null) {
+			throw new NullPointerException("Parameter " + name + " is null");
+		}
+	}
+}
